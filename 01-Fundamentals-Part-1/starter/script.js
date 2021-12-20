1fdbee605e3b4243fb7e6d533926f1da4f7e9cc7
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-20 16:17:45 +0100

    Lectures 1-11

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,9 @@
+// Values and Variables
+
+let country = 'Germany';
+let continent =  'Europe';
+let population = 83.24;
+
+console.log(`${country} is a country in ${continent}, with population of ${population} mln people.`);
+
+// Data Types
