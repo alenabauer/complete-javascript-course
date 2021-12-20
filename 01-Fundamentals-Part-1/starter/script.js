082e35b4b0518f7ba5d0aa08270822ba924e7a40
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-20 16:56:34 +0100

    Practice 12. Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -6,4 +6,12 @@ let population = 83.24;
 
 console.log(`${country} is a country in ${continent}, with population of ${population} mln people.`);
 
+
 // Data Types
+let isIsland = false;
+let language;
+console.log(typeof country);
+console.log(typeof continent);
+console.log(typeof population);
+console.log(typeof isIsland);
+console.log(typeof language);
