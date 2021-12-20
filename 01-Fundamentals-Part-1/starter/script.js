08ecc07117dabbcb3f9be924e2f3cf2fda46c35c
Alena Bauer <alena.a.bauer@gmail.com>
2021-12-20 17:27:20 +0100

    Practice let, const and var

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,17 +1,21 @@
 // Values and Variables
 
-let country = 'Germany';
-let continent =  'Europe';
-let population = 83.24;
+// let country = 'Germany';
+// let continent =  'Europe';
+// let population = 83.24;
 
-console.log(`${country} is a country in ${continent}, with population of ${population} mln people.`);
+// console.log(`${country} is a country in ${continent}, with population of ${population} mln people.`);
 
 
 // Data Types
-let isIsland = false;
-let language;
-console.log(typeof country);
-console.log(typeof continent);
-console.log(typeof population);
-console.log(typeof isIsland);
-console.log(typeof language);
+// let isIsland = false;
+// let language;
+// console.log(typeof country);
+// console.log(typeof continent);
+// console.log(typeof population);
+// console.log(typeof isIsland);
+// console.log(typeof language);
+
+
+// let, const and var
+// const language = 'German';
